2e5a533c0a88483e9f9b75bfbc2c3e78438c027d
Sara García <s.garcia11234@uniandes.edu.co>
2025-05-08 14:31:06 -0500

    componentes

---
+++ src/app/puertas/puertas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-puertas',
+  imports: [],
+  templateUrl: './puertas.component.html',
+  styleUrl: './puertas.component.css'
+})
+export class PuertasComponent {
+
+}
---
+++ src/app/puntos/puntos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-puntos',
+  imports: [],
+  templateUrl: './puntos.component.html',
+  styleUrl: './puntos.component.css'
+})
+export class PuntosComponent {
+
+}
